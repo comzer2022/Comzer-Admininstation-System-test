07cfee7a94cc39b3c9748afd14f2067eee2eed7d
take0819 <sg4053747100@stu.shizugaku.ed.jp>
2026-02-13 12:41:27 +0900

    Create Webhookmanager.js

---
+++ services/Webhookmanager.js
@@ -0,0 +1,25 @@
+// services/webhookManager.js
+import { WebhookClient } from 'discord.js';
+import { ROLE_CONFIG } from '../config/roleConfig.js';
+
+const webhooks = new Map();
+
+export async function getOrCreateHook(channel, roleId) {
+  const key = `${channel.id}:${roleId}`;
+  if (webhooks.has(key)) return webhooks.get(key);
+
+  const whs = await channel.fetchWebhooks();
+  const webhookName = ROLE_CONFIG[roleId].webhookName;
+  const webhookIcon = ROLE_CONFIG[roleId].webhookIcon;
+
+  const existing = whs.find(w => w.name === webhookName);
+  const hook = existing
+    ? new WebhookClient({ id: existing.id, token: existing.token })
+    : await channel.createWebhook({
+      name: webhookName,
+      avatar: webhookIcon,
+    });
+
+  webhooks.set(key, hook);
+  return hook;
+}
