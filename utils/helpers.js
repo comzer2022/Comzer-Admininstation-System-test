4eb0e42ffb80e71c63ea96d00da527c9fe77365c
take0819 <sg4053747100@stu.shizugaku.ed.jp>
2026-02-13 12:45:37 +0900

    Create helpers.js

---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+// utils/helpers.js
+
+export function nowJST() {
+  const now = new Date();
+  return now.toLocaleString('ja-JP', {
+    timeZone: 'Asia/Tokyo',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  });
+}
