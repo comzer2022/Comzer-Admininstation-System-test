522f02abc3f958da454d5b56f701fc6f4dc40969
take0819 <sg4053747100@stu.shizugaku.ed.jp>
2026-02-24 11:57:18 +0900

    Update utils.js

--- commands/blacklist/utils.js
+++ commands/blacklist/utils.js
@@ -1,21 +1,20 @@
 export async function checkPermissions(interaction) {
   let userRoleIds = [];
 
-  if (interaction.guild) {
+  if (interaction.guildId) {
     userRoleIds = interaction.member.roles.cache.map(r => String(r.id));
   } else {
     const refGuildId = "1188411576483590194";
-    if (!refGuildId) {
-      throw new Error("環境変数 REFERENCE_GUILD_ID が設定されていません");
-    }
     const guild = await interaction.client.guilds.fetch(refGuildId);
     const member = await guild.members.fetch(interaction.user.id);
     userRoleIds = member.roles.cache.map(r => String(r.id));
   }
 
+  // EXAMINER_ROLE_IDS を追加
   const ALLOWED_ROLE_IDS = [
-    ...(process.env.ROLLID_MINISTER ? process.env.ROLLID_MINISTER.split(',') : []),
-    ...(process.env.ROLLID_DIPLOMAT ? process.env.ROLLID_DIPLOMAT.split(',') : []),
+    ...(process.env.ROLLID_MINISTER   ? process.env.ROLLID_MINISTER.split(',')   : []),
+    ...(process.env.ROLLID_DIPLOMAT   ? process.env.ROLLID_DIPLOMAT.split(',')   : []),
+    ...(process.env.EXAMINER_ROLE_IDS ? process.env.EXAMINER_ROLE_IDS.split(',') : []),
   ].map(x => x.trim()).filter(Boolean);
 
   const hasRole = ALLOWED_ROLE_IDS.some(roleId => userRoleIds.includes(roleId));
