ae4a71ebfee7153228c0a864d6c40cbe04a159e4
take0819 <sg4053747100@stu.shizugaku.ed.jp>
2026-02-25 23:06:33 +0900

    Update addCountry.js

--- commands/blacklist/addCountry.js
+++ commands/blacklist/addCountry.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';  // ← @discordjs/builders から変更
+import { SlashCommandBuilder } from 'discord.js';
 import { addBlacklistEntry } from '../../utils/blacklistManager.js';
 import { checkPermissions, unauthorizedReply } from './utils.js';
 
