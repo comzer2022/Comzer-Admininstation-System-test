82be3a36e59bb4568f14249a4bf409759a6bf570
take0819 <taketora0819@gmail.com>
2026-08-20 18:08:22 +0900

    Delete config directory

--- config/config.d.ts
+++
@@ -1,8 +0,0 @@
-export interface AppConfig {
-  logChannelId: string;
-  publishChannelId: string;
-  debugChannelId: string;
-  clientId: string;
-  guildId: string;
-  defaultTicketCategory: string;
-}
